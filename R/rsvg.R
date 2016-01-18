c00eb75cb8805ef6e0f3fa95e72b32b827b5a79e
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2016-01-18 22:08:55 +0000

    version 0.3

--- R/rsvg.R
+++ R/rsvg.R
@@ -7,33 +7,104 @@
 #'
 #' @export
 #' @useDynLib rsvg R_rsvg
-#' @param svg path to svg file or raw vector with svg data. Use \link{charToRaw} to convert
-#' strings into raw vectors.
+#' @param svg path/url to svg file or raw vector with svg data. Use \link{charToRaw} to convert
+#' an SVG string into raw data.
 #' @param width output width in pixels or \code{NULL} for default.
 #' @param height output height in pixels or \code{NULL} for default
+#' @rdname rsvg
 #' @examples # create some svg
 #' tmp <- tempfile()
 #' svglite::svglite(tmp, width = 10, height = 7)
 #' ggplot2::qplot(mpg, wt, data = mtcars, colour = factor(cyl))
 #' dev.off()
 #'
-#' # render it into 720p HD
-#' bitmap <- rsvg(tmp, height = 720)
-#' png::writePNG(bitmap, "test.png", dpi = 144)
-#' jpeg::writeJPEG(bitmap, "test.jpg", quality = 1)
-#' webp::write_webp(bitmap, "test.webp", quality = 100)
+#' # render it into a bitmap array
+#' bitmap <- rsvg(tmp, height = 1440)
+#' dim(bitmap) # h*w*c
+#' png::writePNG(bitmap, "bitmap.png", dpi = 144)
+#' jpeg::writeJPEG(bitmap, "bitmap.jpg", quality = 1)
+#' webp::write_webp(bitmap, "bitmap.webp", quality = 100)
+#'
+#' # render straight to output format
+#' rsvg_pdf(tmp, "out.pdf")
+#' rsvg_png(tmp, "out.png")
+#' rsvg_svg(tmp, "out.svg")
+#' rsvg_ps(tmp, "out.ps")
 rsvg <- function(svg, width = NULL, height = NULL) {
+  out <- rsvg_raw(svg, width, height)
+  out <- structure(as.numeric(out)/255, dim = dim(out))
+  aperm(out) # Convert to standard with*height*rgba
+}
+
+#' @rdname rsvg
+#' @export
+rsvg_raw <- function(svg, width = NULL, height = NULL) {
+  svg <- svg_data(svg)
+  stopifnot(is.null(width) || is.numeric(width))
+  stopifnot(is.null(height) || is.numeric(height))
+  out <- .Call(R_rsvg, svg, width, height, 0L)
+  out[c(3,2,1,4),,, drop = FALSE]
+}
+
+#' @rdname rsvg
+#' @export
+rsvg_webp <- function(svg, file = NULL, width = NULL, height = NULL) {
+  out <- rsvg_raw(svg, width, height)
+  webp::write_webp(out, file, 100)
+}
+
+#' @rdname rsvg
+#' @export
+#' @param file path to output file or \code{NULL} to return content as raw vector
+rsvg_png <- function(svg, file = NULL, width = NULL, height = NULL) {
+  rsvg_format(svg, file, width, height, 1L)
+}
+
+#' @rdname rsvg
+#' @export
+rsvg_pdf <- function(svg, file = NULL, width = NULL, height = NULL) {
+  rsvg_format(svg, file, width, height, 2L)
+}
+
+#' @rdname rsvg
+#' @export
+rsvg_svg <- function(svg, file = NULL, width = NULL, height = NULL) {
+  rsvg_format(svg, file, width, height, 3L)
+}
+
+#' @rdname rsvg
+#' @export
+rsvg_ps <- function(svg, file = NULL, width = NULL, height = NULL) {
+  rsvg_format(svg, file, width, height, 4L)
+}
+
+rsvg_format <- function(svg, file = NULL, width = NULL, height = NULL, format = 0) {
+  svg <- svg_data(svg)
+  stopifnot(is.null(width) || is.numeric(width))
+  stopifnot(is.null(height) || is.numeric(height))
+  out <- .Call(R_rsvg, svg, width, height, format)
+  if(is.character(file)){
+    writeBin(out, file[1])
+  } else {
+    return(out)
+  }
+}
+
+svg_data <- function(svg){
   if(is.character(svg)){
-    if(file.exists(svg)){
+    if(grepl("^https?://", svg)){
+      con <- url(svg, "rb")
+      on.exit(close(con))
+      svg <- raw();
+      while(length(buf <- readBin(con, raw(), 1e6))){
+        svg <- c(svg, buf)
+      }
+    } else if(file.exists(svg)){
       svg <- readBin(svg, raw(), file.info(svg)$size)
     } else {
-      stop("Argument 'svg' must be a file path or raw vector.")
+      stop("Argument 'svg' must be a file path, url, or raw vector.")
     }
   }
   stopifnot(is.raw(svg))
-  stopifnot(is.null(width) || is.numeric(width))
-  stopifnot(is.null(height) || is.numeric(height))
-  out <- .Call(R_rsvg, svg, width, height)
-  out <- structure(as.numeric(out)/255, dim = dim(out))
-  aperm(out)[,,c(3,2,1,4)] # Convert to standard with*height*rgba
+  return(svg)
 }
--- src/rsvg.c
+++ src/rsvg.c
@@ -1,9 +1,22 @@
 #include <Rinternals.h>
 #include <librsvg/rsvg.h>
 #include <cairo.h>
+#include <cairo-pdf.h>
+#include <cairo-svg.h>
+#include <cairo-ps.h>
+#include <stdlib.h>
 #include <string.h>
 
-SEXP R_rsvg(SEXP data, SEXP rwidth, SEXP rheight){
+SEXP write_bitmap(RsvgHandle *svg, int width, int height, double sx, double sy);
+SEXP write_png(RsvgHandle *svg, int width, int height, double sx, double sy);
+SEXP write_stream(RsvgHandle *svg, int width, int height, double sx, double sy, cairo_surface_t* (*f) (cairo_write_func_t, void *, double, double));
+
+typedef struct {
+  unsigned char *buf;
+  size_t size;
+} memory;
+
+SEXP R_rsvg(SEXP data, SEXP rwidth, SEXP rheight, SEXP format){
   GError *err = NULL;
   RsvgHandle *svg = rsvg_handle_new_from_data (RAW(data), LENGTH(data), &err);
   if(err != NULL)
@@ -28,13 +41,28 @@ SEXP R_rsvg(SEXP data, SEXP rwidth, SEXP rheight){
   } else if(rwidth != R_NilValue){
     width = asInteger(rwidth);
     sx = sy = width / dimensions.width;
-    height = dimensions.height * sy;
+    height = round(dimensions.height * sy);
   } else {
     height = asInteger(rheight);
     sx = sy = height / dimensions.height;
-    width = dimensions.width * sx;
+    width = round(dimensions.width * sx);
   }
+  switch(asInteger(format)){
+  case 0:
+    return write_bitmap(svg, width, height, sx, sy);
+  case 1:
+    return write_png(svg, width, height, sx, sy);
+  case 2:
+    return write_stream(svg, width, height, sx, sy, cairo_pdf_surface_create_for_stream);
+  case 3:
+    return write_stream(svg, width, height, sx, sy, cairo_svg_surface_create_for_stream);
+  case 4:
+    return write_stream(svg, width, height, sx, sy, cairo_ps_surface_create_for_stream);
+  }
+  return R_NilValue;
+}
 
+SEXP write_bitmap(RsvgHandle *svg, int width, int height, double sx, double sy){
   cairo_surface_t *canvas = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
   cairo_t *cr = cairo_create(canvas);
   cairo_scale(cr, sx, sy);
@@ -55,4 +83,52 @@ SEXP R_rsvg(SEXP data, SEXP rwidth, SEXP rheight){
   cairo_destroy(cr);
   cairo_surface_destroy(canvas);
   return image;
+}
+
+cairo_status_t write_func(void *ctx, const unsigned char *data, unsigned int length) {
+  memory *mem = (memory*) ctx;
+  #ifdef _WIN32
+    mem->buf = realloc(mem->buf, exp2(ceil(log2(mem->size + length))));
+  #else
+    mem->buf = realloc(mem->buf, mem->size + length);
+  #endif
+  if (!mem->buf)
+    return CAIRO_STATUS_WRITE_ERROR;
+  memcpy(&(mem->buf[mem->size]), data, length);
+  mem->size += length;
+  return CAIRO_STATUS_SUCCESS;
+}
+
+SEXP write_png(RsvgHandle *svg, int width, int height, double sx, double sy){
+  cairo_surface_t *canvas = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
+  cairo_t *cr = cairo_create(canvas);
+  cairo_scale(cr, sx, sy);
+  if(!rsvg_handle_render_cairo(svg, cr))
+    Rf_error("Cairo failed to render svg");
+  memory mem = {NULL, 0};
+  cairo_surface_write_to_png_stream(canvas, write_func, &mem);
+  cairo_surface_flush(canvas);
+  cairo_destroy(cr);
+  cairo_surface_destroy(canvas);
+  SEXP res = allocVector(RAWSXP, mem.size);
+  memcpy(RAW(res), mem.buf, mem.size);
+  free(mem.buf);
+  return res;
+}
+
+SEXP write_stream(RsvgHandle *svg, int width, int height, double sx, double sy, cairo_surface_t* (*fun) (cairo_write_func_t, void *, double, double)) {
+  memory buf = {NULL, 0};
+  cairo_surface_t *canvas = fun(write_func, &buf, width, height);
+  cairo_t *cr = cairo_create(canvas);
+  cairo_scale(cr, sx, sy);
+  if(!rsvg_handle_render_cairo(svg, cr))
+    Rf_error("Cairo failed to render svg");
+  cairo_surface_show_page(canvas);
+  cairo_surface_flush(canvas);
+  cairo_destroy(cr);
+  cairo_surface_destroy(canvas);
+  SEXP res = allocVector(RAWSXP, buf.size);
+  memcpy(RAW(res), buf.buf, buf.size);
+  free(buf.buf);
+  return res;
 }
